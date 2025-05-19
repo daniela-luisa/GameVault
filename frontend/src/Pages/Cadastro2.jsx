008054cced0ea81bb6834521c9d5f765b9bf1b8b
Marcelo047 <egermarcelo1@gmail.com>
2025-05-19 20:28:47 -0300

    paginas front iniciadas

--- frontend/src/Pages/Cadastro2.jsx
+++ frontend/src/Pages/Cadastro2.jsx
@@ -1,27 +1,27 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
- export default function Cadastro2() {
-   const [categorias, setCategorias] = useState([]); 
-   const [selecionadas, setSelecionadas] = useState([]);
-   const navigate = useNavigate();
-   const id_usuario = localStorage.getItem('id_usuario');
+export default function Cadastro2() {
+  const [categorias, setCategorias] = useState([]);
+  const [selecionadas, setSelecionadas] = useState([]);
+  const navigate = useNavigate();
+  const id_usuario = localStorage.getItem('id_usuario');
   //  console.log("ID do usuário logado:", id_usuario); 
 
   useEffect(() => {
-  async function carregarCategorias() {
-    try {
-      const response = await fetch('http://localhost:3001/categorias');
-      const dados = await response.json();
-      setCategorias(dados);
-    } catch (error) {
-      console.error('Erro ao buscar categorias:', error);
+    async function carregarCategorias() {
+      try {
+        const response = await fetch('http://localhost:3001/categorias');
+        const dados = await response.json();
+        setCategorias(dados);
+      } catch (error) {
+        console.error('Erro ao buscar categorias:', error);
+      }
     }
-  }
-  carregarCategorias();
-}, []);
+    carregarCategorias();
+  }, []);
 
-const toggleCategoria = (id) => {
+  const toggleCategoria = (id) => {
     if (selecionadas.includes(id)) {
       setSelecionadas(selecionadas.filter(cat => cat !== id));
     } else {
@@ -38,29 +38,67 @@ const toggleCategoria = (id) => {
         categorias: selecionadas
       })
     })
-    .then(response => {
-      if (response.ok) {
-        alert("Categorias salvas com sucesso!");
-        navigate('/home');
-      } else {
-        alert("Erro ao salvar categorias");
-      }
-    })
-    .catch(error => console.error("Erro ao enviar:", error));
+      .then(response => {
+        if (response.ok) {
+          alert("Categorias salvas com sucesso!");
+          navigate('/home');
+        } else {
+          alert("Erro ao salvar categorias");
+        }
+      })
+      .catch(error => console.error("Erro ao enviar:", error));
   };
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h2>Escolha suas categorias favoritas</h2>
+    <div className="min-h-screen flex items-center justify-evenly bg-gradient-to-b from-[#000A05] via-[#002211] to-[#003F1F]">
+      <div className="bg-black/50 rounded-2xl p-10 w-[800px] h-[500px] relative">
       <div>
-        {/* aqui ele vai mapear todas as categorias e por em um botão cada uma delas*/}
-        {categorias.map(categoria => (
-          <button key={categoria.id_catego} onClick={() => toggleCategoria(categoria.id_catego)}
-            style={{ border: selecionadas.includes(categoria.id_catego) ? '2px solid green' : '1px solid gray'}} >  {categoria.nome} </button>
-        ))}
+        <div className="flex items-center justify-between mb-5">
+          <h1 className="text-white text-5xl font-bold tracking-widest" style={{ fontFamily: "Acme" }}>CADASTRO</h1>
+          <div className="flex items-center gap-2">
+            {/* Coloque seu logo aqui, se quiser */}
+            <img src="../Game-removebg-preview.png" alt="Game Vault Logo" className="h-18" />
+          </div>
+        </div>
+        <div className="flex items-center">
+          <h1 className="text-white text-2xl tracking-widest" style={{ fontFamily: "Acme" }}>Escolha suas categorias favoritas</h1>
+        </div>
+        </div>
+
+
+
+       
+          <div className="grid grid-cols-3 gap-3 px-8 mt-3">
+            {/* aqui ele vai mapear todas as categorias e por em um botão cada uma delas*/}
+            {categorias.map(categoria => (
+             <button
+             key={categoria.id_catego}
+             onClick={() => toggleCategoria(categoria.id_catego)}
+             className={`${selecionadas.includes(categoria.id_catego) ? 'bg-green-500' : 'bg-gray-800'} py-2 px-8 rounded text-white`}
+           >
+             {categoria.nome}
+           </button>
+            ))}
+          </div>
+          
+
+          <div className="bottom-0 col-span-2 relative flex items-center mt-12">
+
+            <span className="mt-1 ml-1.5  text-gray-400 hover:text-gray-200 text-sm flex items-center gap-2">&lt;</span>
+            <a href="../cadastro" className="mt-1 ml-1.5 underline text-gray-400 hover:text-gray-200 text-sm flex items-center gap-2">
+              Voltar para Cadastro
+            </a>
+            <div className="flex absolute justify-center w-full pointer-events-none">
+              <button
+                type="submit"
+                className="bg-green-500 hover:bg-green-600 text-white py-2 px-8 rounded-md text-2xl flex items-center gap-2 transition pointer-events-auto"
+                style={{ fontFamily: "Acme" }}>
+                CONTINUAR
+              </button>
+            
+          </div>
+        </div>
       </div>
-      <br />
-      <button onClick={enviarCategorias}>Confirmar Categorias</button>
     </div>
   );
 }
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -2,49 +2,93 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const navigate = useNavigate();
 
-    const Login = async (e) => {
-        e.preventDefault();
-    
-        const resposta = await fetch('http://localhost:3001/login', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ email, senha }),
-        });
-    
-        const dados = await resposta.json();
-        // console.log(dados.cadastro2Completo);
+  const Login = async (e) => {
+    e.preventDefault();
 
-        if (resposta.ok) {
-          alert('Login feito com sucesso!');
-          console.log(dados); 
-          
-          localStorage.setItem('id_usuario', dados.usuario.id);
-          //--------------------------------------------------
-          if (dados.usuario.cadastro2Completo) {
-            console.log('Redirecionando para home...');
-            navigate('/home');
-          } else {
-            console.log('Redirecionando para cadastro2...');
-            navigate('/cadastro2');
-          }
-          //------------------------------------------------------
-        } else {
-          alert(dados.erro || 'Erro ao fazer login');
-        }
-      };
+    const resposta = await fetch('http://localhost:3001/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, senha }),
+    });
 
-      return (
-        // aqui dentro do form ele vai pegar todos os dados dentro dos input e enviar para o metodo post do localhost:3001/login no backend pra poder fazer o controle de login ou seja, cuidado ao mexer
-        <form onSubmit={Login}>
-          <h1>Login</h1>
-          <input type="email" placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-          <input type="password" placeholder="Digite sua senha" value={senha} onChange={(e) => setSenha(e.target.value)}/>
-          <button type="submit">Entrar</button>
+    const dados = await resposta.json();
+    // console.log(dados.cadastro2Completo);
+
+    if (resposta.ok) {
+      alert('Login feito com sucesso!');
+      console.log(dados);
+
+      localStorage.setItem('id_usuario', dados.usuario.id);
+      //--------------------------------------------------
+      if (dados.usuario.cadastro2Completo) {
+        console.log('Redirecionando para home...');
+        navigate('/home');
+      } else {
+        console.log('Redirecionando para cadastro2...');
+        navigate('/cadastro2');
+      }
+      //------------------------------------------------------
+    } else {
+      alert(dados.erro || 'Erro ao fazer login');
+    }
+  };
+
+  return (
+    // aqui dentro do form ele vai pegar todos os dados dentro dos input e enviar para o metodo post do localhost:3001/login no backend pra poder fazer o controle de login ou seja, cuidado ao mexer
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-[#000A05] via-[#002211] to-[#003F1F]">
+      <div className="bg-black/50 rounded-2xl p-10 w-[500px] h-[500px]">
+
+        <div className="flex items-center justify-between mb-4">
+          <h1 className="text-white text-5xl font-bold tracking-widest" style={{ fontFamily: "Acme" }}>LOGIN</h1>
+          <div className="flex items-center gap-2">
+            {/* Coloque seu logo aqui, se quiser */}
+            <img src="../Game-removebg-preview.png" alt="Game Vault Logo" className="h-18" />
+          </div>
+        </div>
+
+        <form onSubmit={Login} className="gap-6 p-8">
+          <div className="flex flex-col">
+            <label className="text-white mb-1">Informe seu emial</label>
+            <input type="email"
+              placeholder="Digite seu email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-gray-800 text-white placeholder-gray-400 p-1.5 rounded-md mb-6" />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-white mb-1">Informe seu email</label>
+            <input type="password"
+              placeholder="Digite sua senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              className="bg-gray-800 text-white placeholder-gray-400 p-1.5 rounded-md mb-1" />
+            <a href="../" className="mt-1 ml-1.5 underline text-gray-400 hover:text-gray-200 text-xs flex items-center gap-2 mb-5">
+              Esqueci minha senha
+            </a>
+          </div>
+
+          <div className="flex flex-col mt-6 items-center">
+           
+           <button
+             type="submit"
+             className="bg-green-500 hover:bg-green-600 text-white py-2 rounded-md text-2xl flex px-8 gap-2 transition pointer-events-auto"
+             style={{ fontFamily: "Jaro" }}>
+             CONTINUAR
+           </button>
+
+            <a href="/cadastro" className="mt-1 ml-1.5 underline text-gray-400 hover:text-gray-200 text-sm flex items-center gap-2">
+              Nâo tenho cadastro
+            </a>
+           
+          </div>
+
         </form>
-      );
+      </div>
+    </div>
+  );
 }
 
