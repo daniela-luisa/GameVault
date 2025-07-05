4f51882eeb391fbee64c858d505b26eae4e2c15a
daniela-luisa <danielaluisaconceicao9063@gmail.com>
2025-07-05 02:49:19 -0300

    pagina de gerenciamento de jogos
    sem estilização porem com as funcionalidades

--- frontend/src/Pages/admin/Jogos.jsx
+++ frontend/src/Pages/admin/Jogos.jsx
@@ -1,4 +1,75 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Jogos() {
+  const [jogo, setJogos] = useState([]);
+  const [categorias, setCategorias] = useState([]);
+  const [jogoSelecionado, setJogoSelecionado] = useState(null);
+  const [mensagem, setMensagem] = useState('');
+  const [sucesso, setSucesso] = useState(null);
+  const [admNome, setAdmNome] = useState('');
+
+const carregarJogos = async () => {
+    try {
+      const resposta = await fetch('http://localhost:3001/admin/jogos');
+      const dados = await resposta.json();
+      setJogos(dados.jogos || []);
+      setAdmNome(dados.admNome || '');
+
+      if (dados.jogos && dados.jogos.length > 0) {
+        carregarCategorias(dados.jogos[0].id_jogo);
+        setJogoSelecionado(dados.jogos[0].id_jogo);
+      }
+    } catch (error) {
+      console.error('Erro ao carregar Jogos:', error);
+    }
+  };
+
+  useEffect(() => {
+    carregarJogos();
+  }, []);
+
+    const excluirJogo = async (id) => {
+    try {
+      const resposta = await fetch(`http://localhost:3001/admin/excluir-jogo/${id}`);
+      const dados = await resposta.json();
+      setMensagem(dados.mensagem);
+      setSucesso(dados.sucesso);
+      if (dados.sucesso) {
+        setJogos(prev => prev.filter(u => u.id_jogo !== id));
+        setCategorias([]);
+        setJogoSelecionado(null);
+      }
+    } catch (error) {
+      console.error('Erro ao excluir Jogo:', error);
+    }
+  };
+ const carregarCategorias = async (id) => {
+    try {
+      const resposta = await fetch(`http://localhost:3001/admin/categorias-jogo/${id}`);
+      const dados = await resposta.json();
+      console.log('Categorias recebidas:', dados);
+      setCategorias(dados.categorias || []);
+      setJogoSelecionado(id);
+    } catch (error) {
+      console.error('Erro ao carregar Categoria:', error);
+    }
+  }
+
+  const excluirCategoria = async (id_jogo, id_categoria) => {
+    try {
+      const resposta = await fetch(`http://localhost:3001/admin/excluir-preferencia/${id_jogo}/${id_categoria}`);
+      const dados = await resposta.json();
+      setMensagem(dados.mensagem);
+      setSucesso(dados.sucesso);
+      if (dados.sucesso) {
+        setCategorias(prev => prev.filter(p => p.id_catego !== id_categoria));
+      }
+    } catch (error) {
+      console.error('Erro ao excluir Categorias:', error);
+    }
+  };
+
   return (
         <div className="min-h-screen bg-gradient-to-b from-[#000A05] via-[#002211] to-[#003F1F] text-white">
       <nav className="bg-black flex justify-between items-center px-6 py-4 text-sm font-semibold text-white">
@@ -13,9 +84,80 @@ export default function Jogos() {
             <div className="max-w-7xl mx-auto p-6">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-2xl font-bold">Gerenciamento de Jogos</h1>
+       <Link to="/admin/novo-usuario">Adicionar Jogo</Link>
         </div>
+
+        {mensagem && (
+          <div style={{ marginBottom: '1rem', padding: '1rem', backgroundColor: sucesso ? 'green' : 'red', color: 'white' }}>
+            {mensagem}
+          </div>
+        )}
+        <h2>Jogos</h2>
+        <table width="100%" style={{ marginBottom: '5.0rem'}}>
+          
+
+        <thead>
+            <tr>
+           <th>Nome</th>
+           <th>Descrição</th>
+           <th>Ações</th>
+           </tr>
+        </thead>
+          <tbody>
+            {jogo.map(jogo => (
+              <tr key={jogo.id_jogo} >
+                <td style={{ cursor: 'pointer' }} onClick={() => carregarCategorias(jogo.id_jogo)}>{jogo.nome}
+                </td>               
+                <td style={{ cursor: 'pointer'}} onClick={() => carregarCategorias(jogo.id_jogo)}>{jogo.descricao}
+                </td>
+                
+                <td>
+                <Link to={`/admin/atualizar-jogo/${jogo.id_jogo}`} style={{ cursor: 'pointer' }}>Editar</Link>
+                <button onClick={() => excluirJogo(jogo.id_jogo)} style={{ cursor: 'pointer' }}>Excluir</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+{jogoSelecionado && (
+  <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+    <h2>Categorias do Jogo Selecionado</h2>
+    <Link to={`/admin/nova-preferencia/${jogoSelecionado}`}>Adicionar Categorias</Link>
+  </div>
+)}
+
+{jogoSelecionado && (
+  <table width="100%">
+   <thead>
+  <tr>
+    <th>Categoria</th>
+    <th>Ações</th>
+  </tr>
+</thead>
+<tbody>
+  {categorias.length > 0 ? (
+    categorias.map((cat, index) => (
+      <tr key={index}>
+        <td>{cat.Categoria}</td>
+        <td>
+          <button
+            onClick={() => excluirCategoria(jogoSelecionado, cat.id_catego)}
+            style={{ cursor: 'pointer' }}>Excluir
+          </button>
+        </td>
+      </tr>
+    ))
+  ) : (
+    <tr>
+      <td colSpan="2">Nenhuma Categoria encontrada.</td>
+    </tr>
+  )}
+</tbody>
+
+  </table>
+)}
       </div>
-      {/* Futuramente: listagem e ações */}
     </div>
   );
 }
